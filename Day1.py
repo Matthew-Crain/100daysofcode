0e49766b1e9a6c16adb22bf91b293aed3822b3d4
Matthew-Crain <85702251+Matthew-Crain@users.noreply.github.com>
2024-02-07 19:41:02 +0000

    Added spacing to line 7 for better readability

---
+++ Day1.py
@@ -0,0 +1,9 @@
+print('this is the band name generator');
+print('please input your home town');
+hometown = input();
+print('please input your pet name');
+petname= input();
+print('AWESOME!');
+print('your band name is: ' + hometown + " " + petname)
+print('keep on rocking')
+exit()
