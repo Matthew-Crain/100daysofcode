17d593a99034e507cb7e7faa40addfaa500f5ea1
Matthew-Crain <85702251+Matthew-Crain@users.noreply.github.com>
2024-02-12 17:43:05 +0000

    completed first 2 if/else statements, need elif

---
+++ Day3_CHOOSE_YOUR_OWN_ADV.py
@@ -0,0 +1,10 @@
+print('welcome to treasure island argh, lets find the treasure')
+choice1 = input('do you want to go left or right?')
+if choice1 == 'left':
+    choice2 = input('You come to the edge of a lake with an island in the middle, a sign says a boat will come soon do you wait or swim?')
+else:
+     print('you werent watching your step and walked into an open lions mouth. GAME OVER')
+
+
+if choice2 = 'wait':
+    choice3 = input('You arrived at the island, there is a hut with 3 doors red, blue, and yellow, which do you choose?')
