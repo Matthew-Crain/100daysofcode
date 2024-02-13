baf17e045ba3e2664996867f74f62b97a67b7f4e
Matthew-Crain <85702251+Matthew-Crain@users.noreply.github.com>
2024-02-13 20:27:22 +0000

    completed logic

--- Day4_ROCKPAPERSCISSORS.py
+++ Day4_ROCKPAPERSCISSORS.py
@@ -26,12 +26,12 @@ scissors = '''
 ---.__(___)
 '''
 computer = random.randint(0,2)
-choice = input("Do you want to play Rock(0),Paper(1), Or Scissors(2)?\n")
-if choice == '0':
+choice = int(input("Do you want to play Rock(0),Paper(1), Or Scissors(2)?\n"))
+if choice == 0:
     print(rock)
-elif choice == '1':
+elif choice == 1:
     print(paper)
-elif choice == '2':
+elif choice == 2:
     print(scissors)
 else:
     print('that is not a valid choice please try again')
@@ -39,10 +39,20 @@ else:
 
 print('VS')
 
-print(computer)
 if computer == 0:
     print(rock)
 elif computer == 1:
     print(paper)
 else:
     print(scissors)
+
+if choice == 0 and computer == 2:
+    print ('YOU WIN')
+elif computer == 0 and choice ==2:
+    print ('YOU LOSE')
+elif choice > computer:
+    print('YOU WIN')
+elif choice == computer:
+    print('ITS A DRAW')
+else:
+    print('YOU LOSE')
