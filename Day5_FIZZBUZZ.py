895c1ddcad14afbc8a8b6dbed08b2852944578a8
Matthew-Crain <85702251+Matthew-Crain@users.noreply.github.com>
2024-02-20 21:18:52 +0000

    fixed logic fizzbuzz works

--- Day5_FIZZBUZZ.py
+++ Day5_FIZZBUZZ.py
@@ -1,16 +1,16 @@
 #create loop
-top = input("how long are we fizzbuzzing till? (enter number)")
+top = input("how long are we fizzbuzzing till? (enter number)\n")
 top = int(top)
 for n in range(1,top + 1):
     #logic for fizz
-    if n % 3 == 0:
-        n = 'fizz'
+    if n % 3 == 0 and n % 5 == 0:
+        n = 'fizzbuzz'
     #logic for buzz
     elif n % 5 == 0:
         n = 'buzz'
     #logic for fizzbuzz
-    elif n % 3 == 0 and n % 5 ==0:
-        n = 'fizzbuzz'
+    elif n % 3 == 0:
+        n = 'fizz'
     else:
         n = n
     print(n)
