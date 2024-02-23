5cbca34c606e282e1bfe015d9d44f7d64400f34f
Matthew-Crain <85702251+Matthew-Crain@users.noreply.github.com>
2024-02-23 21:29:42 +0000

    got replacement of letters working

--- Day6_Hangman.py
+++ Day6_Hangman.py
@@ -4,15 +4,25 @@ import random
 words = ["dog","cat","mouse","house","bog","hat","mat"]
 #Pick a word
 word = random.choice(words)
+#word output for testing
+print(f'the word is: ' + word)
+#create an empty list to hold blanks
+holder = []
+for letters in range(len(word)):
+    holder += '_'
+
 #create an input to guess a letter
-guess = input("please guess a letter: ").lower
+guess = input("please guess a letter: ").lower()
 #loop through guess
-for letter in word:
+
+for position in range(len(word)):
+    letter = word[position]
     if letter == guess:
-        print(letter)
-    else:
-        print('_')
+        holder[position] = guess
 
 
-#print boolean value for correct letter placement
+print(holder)
+
+        
+
 
