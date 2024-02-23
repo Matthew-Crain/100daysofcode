fbe9d918a774b94a1f749c7e0bb3938934ce6c5e
Matthew-Crain <85702251+Matthew-Crain@users.noreply.github.com>
2024-02-23 17:18:04 +0000

    found a way to pick a random word

--- Day6_Hangman.py
+++ Day6_Hangman.py
@@ -1,6 +1,9 @@
+#import libraries
+import random
 #create a word list
 words = ["dog","cat","mouse","house","bog","hat","mat"]
 #Pick a word
+word = random.choice
 #create an input to guess a letter
 #loop through guess
 #print boolean value for correct letter placement
