be0303423d378d6dd03b77a7cf7729639d4b65cc
Matthew-Crain <85702251+Matthew-Crain@users.noreply.github.com>
2024-02-23 18:27:29 +0000

    correct loop for letters

--- Day6_Hangman.py
+++ Day6_Hangman.py
@@ -3,8 +3,16 @@ import random
 #create a word list
 words = ["dog","cat","mouse","house","bog","hat","mat"]
 #Pick a word
-word = random.choice
+word = random.choice(words)
 #create an input to guess a letter
+guess = input("please guess a letter: ").lower
 #loop through guess
+for letter in word:
+    if letter == guess:
+        print(letter)
+    else:
+        print('_')
+
+
 #print boolean value for correct letter placement
 
