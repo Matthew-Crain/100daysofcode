0eb6fcbcf8683fca01202ef892b1cbbd7e14b375
Matthew-Crain <85702251+Matthew-Crain@users.noreply.github.com>
2024-02-23 17:16:05 +0000

    created word list

---
+++ Day6_Hangman.py
@@ -0,0 +1,7 @@
+#create a word list
+words = ["dog","cat","mouse","house","bog","hat","mat"]
+#Pick a word
+#create an input to guess a letter
+#loop through guess
+#print boolean value for correct letter placement
+
