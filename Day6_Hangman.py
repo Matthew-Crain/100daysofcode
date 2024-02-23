53e5fcc16d994177a530608953de7f2705bf02da
Matthew-Crain <85702251+Matthew-Crain@users.noreply.github.com>
2024-02-23 21:50:27 +0000

    added life counter and while loop

--- Day6_Hangman.py
+++ Day6_Hangman.py
@@ -4,24 +4,36 @@ import random
 words = ["dog","cat","mouse","house","bog","hat","mat"]
 #Pick a word
 word = random.choice(words)
+wordln = len(word)
 #word output for testing
 print(f'the word is: ' + word)
 #create an empty list to hold blanks
 holder = []
 for letters in range(len(word)):
     holder += '_'
-
+#create while loop to allow repeatability
+lives = 5
+while lives != 0:
 #create an input to guess a letter
-guess = input("please guess a letter: ").lower()
-#loop through guess
-
-for position in range(len(word)):
-    letter = word[position]
+    print(f'lives: {lives}')
+    print(holder)
+    guess = input("please guess a letter: ").lower()
+    lives -= 1
+    for position in range(wordln):
+        letter = word[position]
     if letter == guess:
         holder[position] = guess
 
+    if '_' not in holder:
+        print('YOU WIN')
+print('YOU LOSE')    
 
-print(holder)
+#loop through guess
+
+
+
+
+p
 
         
 
