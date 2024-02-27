c0cc19bd5b63c40b9eb7e912d88b6da71e91529e
Matthew-Crain <85702251+Matthew-Crain@users.noreply.github.com>
2024-02-27 00:50:20 +0000

    created a few functions to test

---
+++ Day7_function_test.py
@@ -0,0 +1,20 @@
+def greeting():
+    print('hello')
+    print('my name is puter')
+    print('I hope you are well')
+
+#greeting()
+
+#def greet(x = input("what is your name?\n")):
+ #   print(f'hello {x}')
+  #  print(f'how do you do {x}')
+   # print('my name is puter')
+
+#greet()
+    
+def greet_with(x = input("whats your name?\n"),y = input("where are you from?\n")):
+    print(f'hello {x} nice to meet you')
+    print(f'I hear {y} is very nice')
+    print(f'my name is puter')
+
+greet_with()
